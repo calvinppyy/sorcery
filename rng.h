c3d0a8f258e454d6876fbaa08af4ba6c1933c5de
xsq <xusq998@outlook.com>
2019-07-24 14:18:13 -0400

    xsq add rng function

---
+++ rng.h
@@ -0,0 +1,10 @@
+#ifndef RNG_H
+#define RNG_H
+
+#include <ctime>
+#include <cstdlib>
+
+//use this function to generate a random integer under a maximum value
+int generateRandom(int maxValue);
+
+#endif
