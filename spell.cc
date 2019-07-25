f9805d15a9bcd45ec0a9fb2000d879e76dcaa546
Ethnn <812537071qq@gmail.com>
2019-07-25 10:01:46 -0400

    spell update

--- spell.cc
+++ spell.cc
@@ -1,1 +1,40 @@
 #include "spell.h"
+
+void Spell::playCard();
+void Spell::editAttack(int i) {}
+void Spell::editHealth(int i) {}
+void Spell::addEnchantment(std::unique_ptr<Enchantment> &enchantment) {}
+void Spell::startCast(std::unique_ptr<Player> &player, std::unique_ptr<Card> &card);
+void Spell::endCast(std::unique_ptr<Player> &player, std::unique_ptr<Card> &card);
+void Spell::cast(std::unique_ptr<Player> &player, std::unique_ptr<Card> &card);
+void Spell::removeEnchantment(std::unique_ptr<Enchantment> &enchantment) {}
+void Spell::inspect();
+void Spell::print(bool);
+
+std::string Spell::getDescription() {
+    if (name = "Banish") {return "Distroy target minion or ritual"};
+    if (name = "Unsummon") {return "Return target minion to its owner's hand"};
+    if (name = "Recharge") {return "Your ritual gains 3 charges"};
+    if (name = "Disenchant") {return "Distroy the top enchantment on target minion"};
+    if (name = "Raise Dead") {return "Resurrect the top minion in your graveyard and set its defence to 1"};
+    if (name = "Blizzard") {return "Deal 2 damages to all minions"};
+}
+
+int Spell::getHealth() {}
+void Spell::editHealth(int i) {}
+int Spell::getMagic() {
+    return magic;
+}
+
+void Spell::editMagic(int i) {
+    magic += i;
+}
+
+int Spell::getAttack() {}
+void Spell::editAttack(int i) {}
+int Spell::getCost();
+void Spell::editCost(int i);
+int Spell::getUsage();
+void Spell::editUsage(int i);
+Info Spell::getInfo() ;
+void Spell::notify();
--- spell.h
+++ spell.h
@@ -1,8 +1,34 @@
 #ifndef SPELL_H
 #define SPELL_H
+#include "card.h"
 
 class Spell:public Card{
-    
+    std::string name;
+    int magic;
+public:
+    void playCard(&std::unique_ptr<Player>) override;
+    void editAttack(int) override;
+    void editHealth(int) override;
+    void addEnchantment(std::unique_ptr<Enchantment> &) override;
+    void startCast(std::unique_ptr<Player>&, std::unique_ptr<Card>&) override;
+    void endCast(std::unique_ptr<Player>&, std::unique_ptr<Card>&) override;
+    void cast(std::unique_ptr<Player>&, std::unique_ptr<Card>&) override;
+    void removeEnchantment(std::unique_ptr<Enchantment>&) override;
+    void inspect() override;
+    void print(bool) override;
+    std::string getDescription() override;
+    int getHealth() override;
+    void editHealth(int) override;
+    int getMagic() override;
+    void editMagic(int) override;
+    int getAttack() override;
+    void editAttack(int) override;
+    int getCost() override;
+    void editCost(int) override;
+    int getUsage() override;
+    void editUsage(int) override;
+    Info getInfo() override;
+    void notify();
 };
 
 #endif
