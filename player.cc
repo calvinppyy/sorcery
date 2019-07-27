e9ab90dc6cc47e181d4982c08b7fb374945de43f
xsq <xusq998@outlook.com>
2019-07-26 21:48:54 -0400

    xsq dynamic cast fix

--- player.cc
+++ player.cc
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-Player::Player(vector<shared_ptr<Card>> notOut, string name){
+Player::Player(vector<shared_ptr<Card>> notOut, string name)
+{
     this->name = name;
     this->notOut = notOut;
     health = 20;
@@ -12,209 +13,293 @@ Player::Player(vector<shared_ptr<Card>> notOut, string name){
 
 Player::~Player() {}
 
-void Player::giveName(string name) {
+void Player::giveName(string name)
+{
     this->name = name;
 }
 
-void Player::setOpponent(shared_ptr<Player> opponent) {
+void Player::setOpponent(shared_ptr<Player> opponent)
+{
     this->opponent = opponent;
 }
 
-void Player::giveDeck(vector<shared_ptr<Card>> deck) {
+void Player::giveDeck(vector<shared_ptr<Card>> deck)
+{
     this->notOut = deck;
 }
 
-bool Player::died() {
+bool Player::died()
+{
     return this->health <= 0;
 }
 
-shared_ptr<Card> Player::getIthMinion(int index) {
-    if (index == 6) return this->ritual;
+shared_ptr<Card> Player::getIthMinion(int index)
+{
+    if (index == 6)
+        return this->ritual;
     return this->minions.at(index - 1);
 }
 
-void Player::playCard(int index, bool testing) {
-    if (testing) {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) this->editMagic(0);
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
-    } else {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) return;
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
-    }       
-    this->hand.at((index - 1))->playCard();//give two arguments for playCard()
-        if (dynamic_cast<shared_ptr<Enchantment>>(this->hand.at((index - 1))) == nullptr && //bug
-            dynamic_cast<shared_ptr<Spell>>(this->hand.at((index - 1))) == nullptr) { //bug
-            if (dynamic_cast<shared_ptr<Ritual>>(this->hand.at((index - 1))) == nullptr) { //bug
-                if (this->minions.size() < 5) {
-                    this->minions.emplace_back(this->hand.at((index - 1)));
-                    this->minions.erase(this->minions.begin() + (index - 1));
-                    this->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
-                    this->opponent->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
-                }
-            } else {
-                if (this->ritual == nullptr) this->ritual = this->hand.at((index - 1));
+void Player::playCard(int index, bool testing)
+{
+    if (testing)
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            this->editMagic(0);
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
+    }
+    else
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            return;
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
+    }
+    if (dynamic_pointer_cast<Enchantment>(this->hand.at((index - 1))) == nullptr && //bug
+        dynamic_pointer_cast<Spell>(this->hand.at((index - 1))) == nullptr)
+    { //bug
+        if (dynamic_pointer_cast<Ritual>(this->hand.at((index - 1))) == nullptr)
+        { //bug
+            if (this->minions.size() < 5)
+            {
+                this->minions.emplace_back(this->hand.at((index - 1)));
                 this->minions.erase(this->minions.begin() + (index - 1));
+                this->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
+                this->opponent->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
             }
-        } else this->hand.at((index - 1))->playCard(make_shared<Player>(*this), 0);
+        }
+        else
+        {
+            if (this->ritual == nullptr)
+                this->ritual = this->hand.at((index - 1));
+            this->minions.erase(this->minions.begin() + (index - 1));
+        }
+    }
+    else
+        this->hand.at((index - 1))->playCard(make_shared<Player>(*this), 0);
 } //bool is for testing mode
 
-void Player::playCard(int index, shared_ptr<Player> target, int targetIndex, bool testing) {
-    if (testing) {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) this->editMagic(0);
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
-    } else {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) return;
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
-    }       
-    this->hand.at((index - 1))->playCard(target, targetIndex);
-        if (dynamic_cast<shared_ptr<Enchantment>>(this->hand.at((index - 1))) == nullptr && //'shared_ptr<Enchantment> is not a reference or pointer
-            dynamic_cast<shared_ptr<Spell>>(this->hand.at((index - 1))) == nullptr) { //same as above
-            if (dynamic_cast<shared_ptr<Ritual>>(this->hand.at((index - 1))) == nullptr) { //same as above
-                if (this->minions.size() < 5) {
-                    this->minions.emplace_back(this->hand.at((index - 1)));
-                    this->minions.erase(this->minions.begin() + (index - 1));
-                    this->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
-                    this->opponent->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
-                }
-            } else {
-                if (this->ritual == nullptr) this->ritual = this->hand.at((index - 1));
-                this->hand.erase(this->hand.begin() + (index - 1));
+void Player::playCard(int index, shared_ptr<Player> target, int targetIndex, bool testing)
+{
+    if (testing)
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            this->editMagic(0);
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
+    }
+    else
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            return;
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
+    }
+    if (dynamic_pointer_cast<Enchantment>(this->hand.at((index - 1))) == nullptr && //'shared_ptr<Enchantment> is not a reference or pointer
+        dynamic_pointer_cast<Spell>(this->hand.at((index - 1))) == nullptr)
+    { //same as above
+        if (dynamic_pointer_cast<Ritual>(this->hand.at((index - 1))) == nullptr)
+        { //same as above
+            if (this->minions.size() < 5)
+            {
+                this->minions.emplace_back(this->hand.at((index - 1)));
+                this->minions.erase(this->minions.begin() + (index - 1));
+                this->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
+                this->opponent->checkTrigger(TriggerType::minionEnter, make_shared<Player>(*this), this->minions.size());
             }
-        } else this->hand.at((index - 1))->playCard(target, targetIndex);
+        }
+        else
+        {
+            if (this->ritual == nullptr)
+                this->ritual = this->hand.at((index - 1));
+            this->hand.erase(this->hand.begin() + (index - 1));
+        }
+    }
+    else
+        this->hand.at((index - 1))->playCard(target, targetIndex);
 }
 
-void Player::useAbility(int index, bool testing) {
-    if (testing) {
+void Player::useAbility(int index, bool testing)
+{
+    if (testing)
+    {
         int tmp;
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+        {
             tmp = this->magic;
             this->editMagic(0);
         }
-        else {
+        else
+        {
             tmp = this->hand.at((index - 1))->getPlayCost();
-            this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
         }
-    } else {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) return;
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
     }
-    try {
-        this->minions.at((index - 1))->cast(make_shared<Player>(*this), 0); 
+    else
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            return;
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
     }
-    catch(string e) {
-        if (testing) this->editMagic(tmp);
-        else this->editMagic(this->hand.at((index - 1))->getPlayCost());
+    try
+    {
+        this->minions.at((index - 1))->cast(make_shared<Player>(*this), 0);
+    }
+    catch (string e)
+    {
+        if (testing)
+            this->editMagic(tmp);
+        else
+            this->editMagic(this->hand.at((index - 1))->getPlayCost());
     }
 } //bool is for testing mode
 
-void Player::useAbility(int index, shared_ptr<Player> target, int targetIndex, bool testing) {
-    if (testing) {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) this->editMagic(0);
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
-    } else {
-        if (this->magic < this->hand.at((index - 1))->getPlayCost()) return;
-        else this->editMagic(-1*this->hand.at((index - 1))->getPlayCost());
+void Player::useAbility(int index, shared_ptr<Player> target, int targetIndex, bool testing)
+{
+    if (testing)
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            this->editMagic(0);
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
     }
-    this->minions.at((index - 1))->cast(target, targetIndex);   
+    else
+    {
+        if (this->magic < this->hand.at((index - 1))->getPlayCost())
+            return;
+        else
+            this->editMagic(-1 * this->hand.at((index - 1))->getPlayCost());
+    }
+    this->minions.at((index - 1))->cast(target, targetIndex);
 }
 
-void Player::draw() {
-    if (this->notOut.empty()) throw "There is no more card in the deck";
+void Player::draw()
+{
+    if (this->notOut.empty())
+        throw "There is no more card in the deck";
     this->hand.emplace_back(this->notOut.back());
     this->notOut.pop_back();
 }
 
-void Player::discard(int index) {
-    if (index == 6) this->ritual = nullptr;
-    this->hand.erase(this->hand.begin()+(index-1));
+void Player::discard(int index)
+{
+    if (index == 6)
+        this->ritual = nullptr;
+    this->hand.erase(this->hand.begin() + (index - 1));
 }
 
-void Player::inspect(int index,bool testing) {
+void Player::inspect(int index, bool testing)
+{
     this->minions.at(index - 1)->inspect(testing);
-} 
+}
 
-void Player::shuffle() {
-    auto rng = default_random_engine {};
+void Player::shuffle()
+{
+    auto rng = default_random_engine{};
     shuffle(this->notOut.begin(), this->notOut.end(), rng); //DELETE THE RNG CLASS IF THIS WORKS, USED <algorithm> && <random>, INCLUDED IN player.h
 }
 
-void Player::takeAttack(int damage) {
+void Player::takeAttack(int damage)
+{
     this->health -= damage;
-}                            //agianst player
+} //agianst player
 
-void Player::attack(int index) {
+void Player::attack(int index)
+{
     this->opponent->takeAttack(this->minions.at(index - 1)->getAttack());
-}                                 // against player
+} // against player
 
-void Player::takeAttack(int enemyIndex, int damage, int index, int attackType) {
-    this->minions.at(index - 1)->editDefence(-1*damage);
-    if (this->minions.at(index - 1)->getDefence() <= 0) {
+void Player::takeAttack(int enemyIndex, int damage, int index, int attackType)
+{
+    this->minions.at(index - 1)->editDefence(-1 * damage);
+    if (this->minions.at(index - 1)->getDefence() <= 0)
+    {
         this->killMinion((index - 1));
         this->checkTrigger(TriggerType::minionLeave, make_shared<Player>(*this), this->minions.size());
     }
-    if (attackType == 0) {
+    if (attackType == 0)
+    {
         this->attack(index, this->minions.at(index - 1)->getAttack(), enemyIndex, 1);
     }
 } // against minion, attackType indicates if the minion is actively attacking or counter-attack
 
-void Player::attack(int index,int damage, int enemyIndex, int attackType) {
+void Player::attack(int index, int damage, int enemyIndex, int attackType)
+{
     this->opponent->takeAttack(index, this->minions.at(index - 1)->getAttack(), enemyIndex, 0);
-}                             // against minion, the 3rd int indicates if the minion is actively attacking or counter-attack
+} // against minion, the 3rd int indicates if the minion is actively attacking or counter-attack
 
-void Player::killMinion(int index) {
+void Player::killMinion(int index)
+{
     this->graveyard.emplace_back(this->minions.at(index - 1));
     this->minions.erase(this->minions.begin() + (index - 1));
 }
 
-void Player::reviveMinion() {
-    if (this->minions.size() < 5) {
+void Player::reviveMinion()
+{
+    if (this->minions.size() < 5)
+    {
         this->minions.emplace_back(this->graveyard.back());
-        this->minions.back()->editDefence((-1)*this->minions.back()->getDefence());
+        this->minions.back()->editDefence((-1) * this->minions.back()->getDefence());
         this->minions.back()->editDefence(1);
     }
     this->graveyard.pop_back();
 }
 
-void Player::allEditDefence(int value) {
-    for (int i = 0; i < this->minions.size(); i++) {
+void Player::allEditDefence(int value)
+{
+    for (int i = 0; i < this->minions.size(); i++)
+    {
         this->minions.at(i)->editDefence(value);
     }
 }
 
-void Player::checkTrigger(TriggerType type, std::shared_ptr<Player> player, int index) {
-    for (int i = 0; i < this->minions.size(); i++) {
-        this->minions.at(i)->checkTrigger(type,player,index);
+void Player::checkTrigger(TriggerType type, std::shared_ptr<Player> player, int index)
+{
+    for (int i = 0; i < this->minions.size(); i++)
+    {
+        this->minions.at(i)->checkTrigger(type, player, index);
     }
-    this->ritual->checkTrigger(type,player,index);
+    this->ritual->checkTrigger(type, player, index);
 }
 
-void Player::summonCard(int count, string name) {
-    for (int i = 0; i < count ; i++) {
-        if (this->minions.size() == 5) return;
+void Player::summonCard(int count, string name)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (this->minions.size() == 5)
+            return;
         this->minions.emplace_back(make_shared<Minion>(name, this));
     }
 }
 
-shared_ptr<Player> Player::getOpponent() {
+shared_ptr<Player> Player::getOpponent()
+{
     return this->opponent;
 }
 
-void Player::editMagic(int value) {
+void Player::editMagic(int value)
+{
     this->magic += value;
-    if (this->magic > this->magicCap) this->magic = this->magicCap;
+    if (this->magic > this->magicCap)
+        this->magic = this->magicCap;
 }
 
-void Player::unsummonCard(int index) {
+void Player::unsummonCard(int index)
+{
     this->hand.emplace_back(this->minions.at(index - 1));
     this->minions.erase(this->minions.begin() + (index - 1));
 }
 
-void Player::editRitualUsage(int value) {
-    if (this->ritual == nullptr) return;
+void Player::editRitualUsage(int value)
+{
+    if (this->ritual == nullptr)
+        return;
     this->ritual->editUsage(value);
 }
 
-int Player::countMinions() {
+int Player::countMinions()
+{
     return this->minions.size();
 }
-
